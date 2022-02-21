4c838a66bb4bfbd7378cc60b79a11d5b640affd4
boskuv <boris_kv@bk.ru>
2022-02-21 20:51:41 +0300

    feature: add Actor and Film models

--- blog_root/blog_app/models.py
+++ blog_root/blog_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+#class Meta
+class Actor(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    birthday = models.DateField(blank=True, null=True)
+    image = models.ImageField(upload_to ='actors/')
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
+
+
+class Film(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor)
+    rating = models.PositiveSmallIntegerField(blank=True, validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+    image = models.ImageField(upload_to ='films/')
+
+    def __str__(self):
+        return self.title
