49420a7e6d26b7434c4fbe89c49949eddee63641
boskuv <boris_kv@bk.ru>
2022-02-27 23:09:56 +0300

    feature: add views post rating and get actor

--- blog_root/blog_app/urls.py
+++ blog_root/blog_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('actor/<int:id>/', views.actor_detail, name='actor_detail'),
+    path('rate_film/<int:id>/', views.rate_film_form, name="rate_film_form"),
 ]
--- blog_root/blog_app/views.py
+++ blog_root/blog_app/views.py
@@ -1,10 +1,31 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Film, Actor
+from .models import Film, Actor, Rating
+from .forms import RatingForm
 
 
 def index(request):
     films = Film.objects.all()
     context = {
-        'films': films
+        'films': films,
+        'form': RatingForm()
     }
     return render(request, 'blog_app/index.html', context)
+
+
+def actor_detail(request, id):
+    actor_instance = Actor.objects.get(id=id)
+    films = Film.objects.filter(actors=actor_instance)
+    context = {
+        'actor': actor_instance,
+        'films': films
+    }
+    return render(request, 'blog_app/actor.html', context)
+
+
+def rate_film_form(request, id):
+    if request.POST:
+        rate_value = request.POST.get('rate')
+        film_instance = Film.objects.get(id=id)
+        Rating.objects.create(rate=rate_value, film=film_instance)
+        return HttpResponseRedirect('/')
